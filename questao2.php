ee87d7a7ab336151dab6f146e24576b361d6fe21
emderma12 <93205579+emderma12@users.noreply.github.com>
2021-12-14 16:03:27 -0300

    envio da questão 2

---
+++ questao2.php
@@ -0,0 +1,13 @@
+<?php 
+
+$pessoa = array("nome" => "maria clara", "cpf" => "087883456-18", "RG" => "2006089107117", "idade" => "55", "altura" => "1.75" );
+
+foreach ($pessoa as $value){
+	echo $value."<br>";
+}
+
+
+
+
+
+ ?>
